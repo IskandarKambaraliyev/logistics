f459ad6c646c262bbb981d8d7768c9fe67d5f4b5
Iskandar Kambaraliyev <human.aow.official@gmail.com>
2024-05-18 00:46:58 +0500

    advantages api

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 <template>
   <SectionMainHero />
   <SectionMainHowBook />
-  <SectionMainWhyBook />
+  <SectionMainAdvantages />
   <SectionMainCost />
   <SectionMainBrands />
   <SectionMainServices />
