7654ff1a246cbaedadd42b48fe888c463b7a518e
IskandarKambaraliyev <human.github@gmail.com>
2024-05-20 18:17:39 +0500

    blog & pages detail

--- pages/blog/[blogId].vue
+++ pages/blog/[blogId].vue
@@ -1,12 +1,22 @@
 <script setup>
   const route = useRoute();
-  const blogId = route.params.blogId;
+  const { blogId } = route.params;
+
+  const { data, error } = await useMyFetch(`/blog/id/${blogId}`);
+
+  console.log(data.value);
+
+  if (error.value) {
+    throw createError({
+      statusCode: error.value.statusCode,
+      message: error.value.message,
+    });
+  }
 </script>
 
 <template>
-  <div>
-    {{ blogId }}
-  </div>
+  <BaseBanner :title="data.title" label="Pages" :image="data.image" />
+  <BaseDetail :data="data" class="mb" />
 </template>
 
 <style lang="scss" scoped></style>
--- pages/pages/[slug].vue
+++ pages/pages/[slug].vue
@@ -1,12 +1,22 @@
 <script setup>
   const route = useRoute();
   const { slug } = route.params;
+
+  const { data, error } = await useMyFetch(`/pages/${slug}`);
+
+  console.log(data.value);
+
+  if (error.value) {
+    throw createError({
+      statusCode: error.value.statusCode,
+      message: error.value.message,
+    });
+  }
 </script>
 
 <template>
-  <div>
-    {{ slug }}
-  </div>
+  <BaseBanner :title="data.title" label="Pages" :image="data.image" />
+  <BaseDetail :data="data" class="mb" />
 </template>
 
 <style lang="scss" scoped></style>
